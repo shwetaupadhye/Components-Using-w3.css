c231f6bffe02ec760c39fcfa759a433488204459
Shweta Upadhye <shwetauoadhye777@gmail.com>
2021-06-13 11:48:53 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { ImageComponent } from './image/image.component';
+import { SectionComponent } from './section/section.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    ImageComponent,
+    SectionComponent,
+    QuotesComponent,
+    ContactComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
